6f03e005bc14a15e02baf86079f833ce9b789e5a
frebbles <frebbles@hotmail.com>
2020-07-06 14:47:38 +1000

    Update to startup file

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#Check Workdir exists prior to running
+if [ ! -d ./workdir ] then
+  mkdir workdir
+fi
 
 docker run -ti --rm --device /dev/ttyUSB0:/dev/ttyUSB0 -v /home/farran/dev/docker-esp-idf/workdir:/workdir espdev
